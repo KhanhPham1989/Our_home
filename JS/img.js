9300fa1f6a7d48be6dd7ea9e9e684188f5103904
KhanhPham1989 <87858812+KhanhPham1989@users.noreply.github.com>
2021-09-19 19:38:17 +0700

    no

--- JS/img.js
+++ JS/img.js
@@ -1,14 +1,48 @@
+$ = document.querySelector.bind(document);
+$$ = document.querySelectorAll.bind(document);
+const boxes = document.querySelectorAll('.box');
+const app = {
+    currentIndex : 0, // lay ra chi muc dau tien cua mang
+   
 
-const boxes = document.querySelectorAll('.box')
+    images : [
+      {
+        id : 'a0',
+        image: './IMG/0.jpg' ,
+      },
+      {
+        id : 'a1',
+        image: './IMG/1.jpg' ,
+      },
+      {
+        id : 'a2',
+        image: './IMG/2.jpg' ,
+      },
+      {
+        id : 'a3',
+        image: './IMG/3.jpg' ,
+      },
+      {
+        id : 'a4',
+        image: './IMG/4.jpg' ,
+      },
+    ],
 
-window.addEventListener('scroll', checkBoxes)
+render: function() {
+    const htmls = this.images.map ((images, id) => {
+        return `
+            <div class="box1"  data-index="${id}" style="background-image: url('${images.image}')"></div>
+           
+        `
+  })
+ boxes.innerHTML = htmls.join('')
+},
 
-checkBoxes()
+show : function checkBoxes() {
+        window.addEventListener('scroll', checkBoxes)
+         const triggerBottom = window.innerHeight / 5 * 4
 
-function checkBoxes() {
-    const triggerBottom = window.innerHeight / 5 * 4
-
-    boxes.forEach(box => {
+         boxes.forEach(box => {
         const boxTop = box.getBoundingClientRect().top
 
         if(boxTop < triggerBottom) {
@@ -17,5 +51,14 @@ function checkBoxes() {
             box.classList.remove('show')
         }
     })
+},
+start : function() {
+    
+    this.show()
+    
+    // Render playlist
+      this.render()
+  }
 };
 
+app.start()
